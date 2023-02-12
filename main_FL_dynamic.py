87cf666695e1104c8573f3817b1df0f4c0bcf1b2
Jiefei Liu <jiefei@nmsu.edu>
2023-02-12 14:52:41 -0700

    update

--- main_FL_dynamic.py
+++ main_FL_dynamic.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     # --------------------Data Loading-----------------------
     # data_dir = "/home/jliu/DoD_Misra_project/jiefei_liu/DOD/CICDDoS2019/"
     data_dir = "/home/jliu/DoD_Misra_project/jiefei_liu/DOD/CICDDoS2019/"
-    pickle_dir = "/home/jliu/DoD_Misra_project/jiefei_liu/DOD/MLP_model/data/partition_low_9_high_9.pkl"
+    pickle_dir = "/home/jliu/DoD_Misra_project/jiefei_liu/DOD/MLP_model/data/partition.pkl"
     num_classes = 11
     print("Loading data...")
     (x_train_un_bin, y_train_un_bin), (x_test, y_test_bin) = data_preprocessing.read_2019_data(data_dir)
