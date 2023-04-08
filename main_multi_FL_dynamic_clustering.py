9b77fab7ec12d445e116a8238057252fd9afa1fc
Jiefei Liu <jiefei@nmsu.edu>
2023-04-08 12:56:36 -0600

    update

--- main_multi_FL_dynamic_clustering.py
+++ main_multi_FL_dynamic_clustering.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     # pickle_dir = "/Users/jiefeiliu/Documents/DoD_Misra_project/jiefei_liu/DOD/MLP_model/data/partition_attacks_2.pkl"
     data_dir = "/home/jliu/DoD_Misra_project/jiefei_liu/DOD/CICDDoS2019/"
     pickle_dir = "/home/jliu/DoD_Misra_project/jiefei_liu/DOD/MLP_model/data/partition_attacks_2.pkl"
-    num_classes = 11
+    num_classes = 12
     print("Loading data...")
     (x_train_un_bin, y_train_un_bin), (x_test, y_test_bin) = data_preprocessing.read_2019_data(data_dir)
     # Load partitioned data
