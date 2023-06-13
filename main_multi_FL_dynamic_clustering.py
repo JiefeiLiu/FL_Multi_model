e9b6a20f2a754cc5caa8fcf2b983f56868627f53
Jiefei Liu <jiefei@nmsu.edu>
2023-06-13 08:31:42 -0600

    update

--- main_multi_FL_dynamic_clustering.py
+++ main_multi_FL_dynamic_clustering.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     learning_rate = 0.01
     batch_size = 64
     # Server hyperparameter setting
-    num_clients = 15
+    num_clients = 20
     training_data_name = str(num_clients) + '_training.pkl'
     rounds = 20
     fraction = 1.0
