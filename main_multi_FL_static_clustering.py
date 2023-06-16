c2a6b155513ec0dd2b5321505a0b2fbedd4c9291
Jiefei Liu <jiefei@nmsu.edu>
2023-06-16 16:20:02 -0600

    update

--- main_multi_FL_static_clustering.py
+++ main_multi_FL_static_clustering.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     learning_rate = 0.01
     batch_size = 64
     # Server hyperparameter setting
-    num_clients = 30
+    num_clients = 20
     training_data_name = str(num_clients) + '_training.pkl'
     rounds = 20
     fraction = 1.0
